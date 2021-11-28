bd873d3ccef1d01fe21140300f4baf6821fbcd04
pokepara <116285754+pokepara@users.noreply.github.com>
2021-11-28 06:24:22 +0000

    Some fix.

--- src/StanfordCoreNlpDemo.java
+++ src/StanfordCoreNlpDemo.java
@@ -3,4 +3,6 @@ import java.io.*;
 import java.util.*;
 
 import edu.stanford.nlp.dcoref.CorefChain;
-import edu.stanford.nlp.dcoref.CorefCoreAnno
+import edu.stanford.nlp.dcoref.CorefCoreAnnotations;
+import edu.stanford.nlp.io.*;
+import edu.stanf
