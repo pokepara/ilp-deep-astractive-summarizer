cd6b9b582f2a2e3213a3de7fdc5732e62b2a26a6
pokepara <116285754+pokepara@users.noreply.github.com>
2021-11-30 13:56:55 +0000

    js

--- src/StanfordCoreNlpDemo.java
+++ src/StanfordCoreNlpDemo.java
@@ -8,4 +8,4 @@ import edu.stanford.nlp.io.*;
 import edu.stanford.nlp.ling.*;
 import edu.stanford.nlp.pipeline.*;
 import edu.stanford.nlp.semgraph.SemanticGraph;
-import 
+import edu.stanford.nlp.semgraph.SemanticGraph
