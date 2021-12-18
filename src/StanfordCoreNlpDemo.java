cfb7b82df56c986668f35bfa6cb41693cd6583d6
pokepara <116285754+pokepara@users.noreply.github.com>
2021-12-18 19:12:40 +0000

    Release new version

--- src/StanfordCoreNlpDemo.java
+++ src/StanfordCoreNlpDemo.java
@@ -32,4 +32,5 @@ public class StanfordCoreNlpDemo {
 
         // Create a CoreNLP pipeline. To build the default pipeline, you can just use:
         //   StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
-        // Here's a more complex se
+        // Here's a more complex setup example:
+        //   Properties props = ne
