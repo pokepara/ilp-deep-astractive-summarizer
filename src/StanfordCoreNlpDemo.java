6d60093cfe2d05eec0aa3e00e6d52a69be8f24b1
pokepara <116285754+pokepara@users.noreply.github.com>
2021-12-22 08:41:56 +0000

    -ignore

--- src/StanfordCoreNlpDemo.java
+++ src/StanfordCoreNlpDemo.java
@@ -35,4 +35,4 @@ public class StanfordCoreNlpDemo {
         // Here's a more complex setup example:
         //   Properties props = new Properties();
         //   props.put("annotators", "tokenize, ssplit, pos, lemma, ner, depparse");
-        
+        //   props.put("ner.model", "edu/stanford/nlp/
