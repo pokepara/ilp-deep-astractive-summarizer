e71d4b3e0d6559505fa7147331d587fddfe2b3cb
pokepara <116285754+pokepara@users.noreply.github.com>
2022-02-28 20:38:36 +0000

    less

--- src/StanfordCoreNlpDemo.java
+++ src/StanfordCoreNlpDemo.java
@@ -83,4 +83,5 @@ public class StanfordCoreNlpDemo {
             out.println(sentence.toShorterString());
             out.println();
             out.println("The first sentence tokens are:");
-            for (CoreMap token : sentence.get(Cor
+            for (CoreMap token : sentence.get(CoreAnnotations.TokensAnnotation.class)) {
+            
