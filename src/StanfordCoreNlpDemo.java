c8c2c5dcc71e5d5d71bb61c9306353487044bd1d
pokepara <116285754+pokepara@users.noreply.github.com>
2022-03-24 09:19:53 +0000

    Add bower.json file

--- src/StanfordCoreNlpDemo.java
+++ src/StanfordCoreNlpDemo.java
@@ -92,4 +92,4 @@ public class StanfordCoreNlpDemo {
             tree.pennPrint(out);
             out.println();
             out.println("The first sentence basic dependencies are:");
-            out.println(sentence.get(SemanticGraphCoreAnnotati
+            out.println(sentence.get(SemanticGraphCoreAnnotations.BasicDependenciesAnnotation.class).toString(Sem
