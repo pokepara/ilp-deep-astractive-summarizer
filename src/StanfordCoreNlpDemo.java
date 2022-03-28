7c02f8d6f0e40ccbfb8db8023e9d0bd43e5832d6
pokepara <116285754+pokepara@users.noreply.github.com>
2022-03-28 20:55:46 +0000

    remove extra spaces

--- src/StanfordCoreNlpDemo.java
+++ src/StanfordCoreNlpDemo.java
@@ -94,4 +94,4 @@ public class StanfordCoreNlpDemo {
             out.println("The first sentence basic dependencies are:");
             out.println(sentence.get(SemanticGraphCoreAnnotations.BasicDependenciesAnnotation.class).toString(SemanticGraph.OutputFormat.LIST));
             out.println("The first sentence collapsed, CC-processed dependencies are:");
-            SemanticGraph graph = senten
+            SemanticGraph graph = sentence.get(SemanticGraphCoreAnnotations.CollapsedCCProcessedDep
