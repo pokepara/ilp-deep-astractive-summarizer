bf4d350f398846c8c7cca43465b81bd0360e434d
pokepara <116285754+pokepara@users.noreply.github.com>
2022-04-13 18:29:50 +0000

    fix gen dual

--- src/StanfordCoreNlpDemo.java
+++ src/StanfordCoreNlpDemo.java
@@ -102,4 +102,4 @@ public class StanfordCoreNlpDemo {
             // along with a method for getting the most representative mention.
             // Both sentence and token offsets start at 1!
             out.println("Coreference information");
-            Map<Inte
+            Map<Integer, edu.stanford.nlp.hcoref.data.CorefChain> co
