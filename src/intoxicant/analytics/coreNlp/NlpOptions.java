dad57b4f7dc3bbfd7e672dd0c7d4c2055d2c3f01
pokepara <116285754+pokepara@users.noreply.github.com>
2022-05-16 17:52:22 +0000

    next commit

--- src/intoxicant/analytics/coreNlp/NlpOptions.java
+++ src/intoxicant/analytics/coreNlp/NlpOptions.java
@@ -4,3 +4,39 @@ package intoxicant.analytics.coreNlp;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 
 import java.util.Properties;
+
+/**
+ * User: jconwell
+ * Date: 5/17/13
+ * Helper class to setup the options for the Stanford NLP engine
+ */
+public class NlpOptions {
+
+    /**
+     * Create NlpOptions configuration class to create a NLP analyzer that only does term tokenization
+     * @param lemmatisation flag to turn lemmatisation on / off during tokenization
+     */
+    public static NlpOptions tokenizationOnly(boolean lemmatisation) {
+        return new NlpOptions(lemmatisation, false, false, false, false, -1, false);
+    }
+
+    /**
+     * Create NlpOptions configuration class to create a NLP analyzer that does named entity recognition,
+     * but does NOT run name disambiguation or coreference analysis
+     */
+    public static NlpOptions namedEntityRecognition(boolean regexNER, boolean sentenceParser) {
+        return new NlpOptions(true, true, regexNER, sentenceParser, false, -1, false);
+    }
+
+    /**
+     * Create NlpOptions configuration class to create a NLP analyzer that does named entity recognition
+     * coreference analysis.
+     * @param corefMaxSentenceDist max sentence distance to evaluate coreference between tokens
+     * @param corefPostProcessing do post procesing of coreference data to trim out singletons
+     */
+    public static NlpOptions namedEntitiesWithCoreferenceAnalysis(boolean regexNER, int corefMaxSentenceDist, boolean corefPostProcessing) {
+        return new NlpOptions(true, true, regexNER, true, true, corefMaxSentenceDist, corefPostProcessing);
+    }
+
+    /**
+     * Create NlpOptions configuration class to create a NLP analyzer that does sentence parsing
