3e2ad914e6e51047678f3a420924d0e79f30aa84
pokepara <116285754+pokepara@users.noreply.github.com>
2022-06-16 22:53:29 +0000

    Updating translations for locale/fr/BOINC-Manager.po [skip ci]

--- src/jaist/summarization/AnnotatorHub.java
+++ src/jaist/summarization/AnnotatorHub.java
@@ -28,4 +28,6 @@ public class AnnotatorHub {
 
             if (inputStream != null) {
                 props.load(inputStream);
-      
+            }
+        }catch(Exception e){
+            Sys
