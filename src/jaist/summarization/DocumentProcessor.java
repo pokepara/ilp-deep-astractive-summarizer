a6fc99990be4e484c8255b6d2cfc339adbf38927
pokepara <116285754+pokepara@users.noreply.github.com>
2022-07-07 09:19:18 +0000

    updates to website

--- src/jaist/summarization/DocumentProcessor.java
+++ src/jaist/summarization/DocumentProcessor.java
@@ -41,3 +41,59 @@ public class DocumentProcessor {
     }
 
     public void processDocuments(File[] fileNames) throws IOException{
+        for (File filepath: fileNames){
+            if (filepath.getName().startsWith(".")) continue;
+            System.out.println(filepath.getAbsolutePath());
+
+            String text = IOUtils.slurpFile(filepath);
+            processDocument(text);
+        }
+
+        removeRedundantCorefs();
+    }
+
+    public void processDocument(String text){
+        InputDocument inputDocument = new InputDocument(text, isDucData);
+        this.docs.add(inputDocument);
+        extractPhrases(inputDocument);
+        this.corefs.putAll(inputDocument.getCoreferences());
+    }
+
+    private void extractPhrases(InputDocument inputDocument){
+        PhraseExtractor extractor = new PhraseExtractor(inputDocument, indicatorMatrix);
+        List<Phrase> phrases = extractor.extractAllPhrases();
+
+        for (Phrase phrase : phrases) {
+            if (phrase.isNP()) {
+                nounPhrases.add(phrase);
+                nouns.add(phrase.getContent());
+            } else {
+                verbPhrases.add(phrase);
+                verbs.add(phrase.getContent());
+            }
+            allPhrases.add(phrase);
+        }
+    }
+
+    private void removeRedundantCorefs(){
+        Iterator<Map.Entry<String, HashSet<String>>> iter = corefs.entrySet().iterator();
+
+        while(iter.hasNext()){
+            Map.Entry<String, HashSet<String>> entry = iter.next();
+            Set<String> mentions = entry.getValue();
+            HashSet<String> newMentions = new HashSet<>(mentions);
+            for(String mention: mentions){
+                if (!nouns.contains(mention)){
+                    newMentions.remove(mention);
+                }
+            }
+
+            if (newMentions.size() < 2){
+                iter.remove();
+            }else{
+                entry.setValue(newMentions);
+            }
+        }
+    }
+
+    public HashMap<String, HashSet<String>> getCorefs(){
