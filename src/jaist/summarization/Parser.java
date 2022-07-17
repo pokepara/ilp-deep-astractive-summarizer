3b62abefa6553071d214e20490daa6983d406aae
pokepara <116285754+pokepara@users.noreply.github.com>
2022-07-17 17:14:16 +0000

    neighbour: fix a race in neigh_destroy()

--- src/jaist/summarization/Parser.java
+++ src/jaist/summarization/Parser.java
@@ -41,3 +41,45 @@ public class Parser {
     HashMap<Integer, GRBVar> verbVariables;
     HashMap<String, GRBVar> gammaVariables;
     HashMap<String, GRBVar> nounToNounVariables;
+    HashMap<String, GRBVar> verbToVerbVariables;
+
+    HashSet<String> nouns;
+    HashSet<String> verbs;
+
+    DocumentProcessor processor;
+
+    static int DEFAULT_MAXIMUM_SENTENCE = 10;
+    static double DEFAULT_ALTERNATIVE_VP_THRESHOLD = 0.75;
+    static int DEFAULT_MAX_WORD_LENGTH = 100;
+    static int MIN_SENTENCE_LENGTH = 5;
+    static int MINIMUM_VERB_LENGTH = 2;
+
+    int max_sentence = 10;
+    double alternative_vp_threshold = 0.75;
+    int max_word_length = 100;
+
+    int threads = 0;
+
+    long previousMarkedTime;
+
+    public Parser(int max_sentence, double alternative_vp_threshold, int max_word_length, int threads, boolean isDucData){
+        this(max_sentence, alternative_vp_threshold, max_word_length, isDucData);
+        this.threads = threads;
+    }
+
+    public Parser(int max_sentence, double alternative_vp_threshold, int max_word_length, boolean isDucData) {
+        this.max_sentence = max_sentence;
+        this.alternative_vp_threshold = alternative_vp_threshold;
+        this.max_word_length = max_word_length;
+
+        this.props = new Properties();
+        pipeline = AnnotatorHub.getInstance().getPipeline();
+        indicatorMatrix = new PhraseMatrix();
+        compatibilityMatrix = new PhraseMatrix();
+        alternativeVPs = new PhraseMatrix();
+        alternativeNPs = new PhraseMatrix();
+
+        nounPhrases = new ArrayList<>();
+        verbPhrases = new ArrayList<>();
+        allPhrases = new ArrayList<>();
+        corefs = new HashMap<>();
