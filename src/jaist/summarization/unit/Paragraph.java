d4e77d97efd53486fbc0281aa042483c80d357ec
pokepara <116285754+pokepara@users.noreply.github.com>
2023-04-19 21:44:22 +0000

    Update menu.yml

--- src/jaist/summarization/unit/Paragraph.java
+++ src/jaist/summarization/unit/Paragraph.java
@@ -6,4 +6,6 @@ import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.EntityMentionsAnnotator;
 import edu.stanford.nlp.util.CollectionUtils;
-import edu.
+import edu.stanford.nlp.util.CoreMap;
+
+import jaist.summarization.
