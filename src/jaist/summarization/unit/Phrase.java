3cc91e0741f6ba12fbdc29824ded6b8316e09e81
pokepara <116285754+pokepara@users.noreply.github.com>
2023-05-25 12:14:59 +0000

    Format changes

--- src/jaist/summarization/unit/Phrase.java
+++ src/jaist/summarization/unit/Phrase.java
@@ -2,4 +2,5 @@ package jaist.summarization.unit;
 
 import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.ling.CoreLabel;
-import edu.stanford.nlp.pipeline.Annot
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.util.CollectionUtil
