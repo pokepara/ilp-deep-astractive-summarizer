f56bd752b15443d109eb586cbeb30d66f3d4330f
pokepara <116285754+pokepara@users.noreply.github.com>
2023-09-11 09:28:22 +0000

    Bumping version for release.

--- src/jaist/summarization/utils/ModelExporter.java
+++ src/jaist/summarization/utils/ModelExporter.java
@@ -3,4 +3,5 @@ package jaist.summarization.utils;
 import jaist.summarization.InputDocument;
 import jaist.summarization.PhraseMatrix;
 import jaist.summarization.unit.Paragraph;
-import jaist.
+import jaist.summarization.unit.Phrase;
+import org.w3c.do
