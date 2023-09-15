43e06d3c1a0e3ad395dcf29d5319cb0859a0ddfd
pokepara <116285754+pokepara@users.noreply.github.com>
2023-09-15 23:08:13 +0000

    tag fix

--- src/jaist/summarization/utils/ModelExporter.java
+++ src/jaist/summarization/utils/ModelExporter.java
@@ -4,4 +4,7 @@ import jaist.summarization.InputDocument;
 import jaist.summarization.PhraseMatrix;
 import jaist.summarization.unit.Paragraph;
 import jaist.summarization.unit.Phrase;
-import org.w3c.do
+import org.w3c.dom.*;
+
+import javax.xml.parsers.DocumentBuilder;
+impor
