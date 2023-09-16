db4b0313a741d4d893f3cb16a659277efb563180
pokepara <116285754+pokepara@users.noreply.github.com>
2023-09-16 21:04:38 +0000

    [MOV] move addons out of __unported__ (they remain not installable)

--- src/jaist/summarization/utils/ModelExporter.java
+++ src/jaist/summarization/utils/ModelExporter.java
@@ -8,4 +8,6 @@ import org.w3c.dom.*;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationE
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
+import ja
