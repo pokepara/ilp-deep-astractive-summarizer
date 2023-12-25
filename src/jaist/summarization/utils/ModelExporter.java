4fb0350e4500caf310dc92bbc84a5c5d63ac972a
pokepara <116285754+pokepara@users.noreply.github.com>
2023-12-25 17:30:31 +0000

    Lines authored by ritzau

--- src/jaist/summarization/utils/ModelExporter.java
+++ src/jaist/summarization/utils/ModelExporter.java
@@ -107,4 +107,4 @@ public class ModelExporter {
                 Element docElement = xmlDoc.createElement("doc");
                 rootElement.appendChild(docElement);
 
-        
+                for (Paragraph paragraph: doc.getP
