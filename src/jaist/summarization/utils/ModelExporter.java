6d6889dd637c222e0838517d6c3aaf6435f6ffb4
pokepara <116285754+pokepara@users.noreply.github.com>
2024-02-07 20:28:38 +0000

    Close #10

--- src/jaist/summarization/utils/ModelExporter.java
+++ src/jaist/summarization/utils/ModelExporter.java
@@ -143,4 +143,5 @@ public class ModelExporter {
 
         StreamResult result = new StreamResult(new File(filename));
         try{
-            transformer.tr
+            transformer.transform(source, result);
+        }catch(Exceptio
