a1d82f791331b3547115aeb14de0291d4e85f0f9
pokepara <116285754+pokepara@users.noreply.github.com>
2024-03-04 10:35:11 +0000

    Updated prerequisites.md

--- src/jaist/summarization/webservice/SummarizationService.java
+++ src/jaist/summarization/webservice/SummarizationService.java
@@ -3,4 +3,9 @@ package jaist.summarization.webservice;
 import jaist.summarization.Parser;
 
 import javax.jws.WebMethod;
-import javax.jws.WebServi
+import javax.jws.WebService;
+
+/**
+ * Created by chientran on 3/6/16.
+ */
+@WebServ
